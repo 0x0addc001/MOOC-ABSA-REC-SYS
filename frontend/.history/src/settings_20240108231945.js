0fa6c1980202c47819684b4e5b929936b3f867f3
Deepen <109563770+TensorTorcher@users.noreply.github.com>
2024-01-09 01:56:01 +0800

    first commit

---
+++ frontend/.history/src/settings_20240108231945.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+  title: '慕课评论方面级情感分析与推荐系统',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: false
+}
